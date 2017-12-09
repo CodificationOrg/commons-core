f300544aea6748c1b064b07f785108ff7697f214
William Weiss <william@codification.org>
2017-12-09 18:03:24 -0500

    Initial work on entry point.

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var S3Utils_1 = require("./S3Utils");
+exports.S3Utils = S3Utils_1.S3Utils;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { S3Utils } from './S3Utils';
