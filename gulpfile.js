1196e67beb5e879edc068fa889f0ff2b67399770
William Weiss <william@codification.org>
2018-08-10 16:18:42 -0500

    Test results now stored as xml doc.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ const ts = require('gulp-typescript');
 
 const paths = {
     src: 'src',
-    dist: 'lib'
+    dist: 'lib',
+    test: 'test-results/tap'
 };
 
 const tsProject = ts.createProject('tsconfig.json');
@@ -14,6 +15,15 @@ const clean = () => {
     return del([paths.dist]);
 }
 exports.clean = clean;
+
+const prepareTest = () => {
+    return gulp
+        .src('package.json', {
+            read: false
+        })
+        .pipe(gulp.dest(paths.test));
+};
+exports.prepareTest = prepareTest;
 
 const transpile = () => {
     var tsResult = gulp.src(`${paths.src}/**/*.ts`)
