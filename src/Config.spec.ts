4540fee7217f62f66c63f6b3ff85562f8230fa54
William Weiss <william@codification.org>
2018-09-18 21:25:51 -0500

    Updating unit tests.

--- src/Config.spec.ts
+++ src/Config.spec.ts
@@ -1,10 +1,28 @@
 import * as test from 'tape';
+
 import { Config } from './Config';
 
-test('Config Unit Tests', t => {
+// tslint:disable:no-string-literal
+test('Config Unit Tests', assert => {
   const expected = process.env['foo'];
-  t.is(Config.get('foo'), expected ? expected : '', 'correctly returns environment or default value');
+  assert.is(Config.get('foo'), expected ? expected : '', 'correctly returns environment or default value');
   Config.put('foo', 'bar');
-  t.is(Config.get('foo'), 'bar', 'correctly returns runtime override value');
-  t.end();
+  assert.is(Config.get('foo'), 'bar', 'correctly returns runtime override value');
+  try {
+    Config.getRequired('hubbajubbasubba', 'Nope, not there!');
+    assert.fail('should have thrown error with supplied message.');
+  } catch (error) {
+    assert.equal(error.message, 'Nope, not there!', 'throws error for missing value with supplied message.');
+  }
+  try {
+    Config.getRequired('hubbajubbasubba');
+    assert.fail('should have thrown error with default message.');
+  } catch (error) {
+    assert.equal(
+      error.message,
+      'Required config value [hubbajubbasubba] is missing.',
+      'throws error for missing value with default message.',
+    );
+  }
+  assert.end();
 });
--- src/Logging/LoggerFactory.spec.ts
+++ src/Logging/LoggerFactory.spec.ts
@@ -1,21 +1,22 @@
 import * as test from 'tape';
+
 import { Level } from './Level';
 import { LoggerFactory } from './LoggerFactory';
 
-test('LoggerFactory Unit Tests', t => {
+test('LoggerFactory Unit Tests', assert => {
   const logger = LoggerFactory.getLogger('Foo');
   LoggerFactory.setLevel(logger, Level.INFO);
 
-  t.is(logger.name, 'Foo', 'can create a logger');
-  t.is(logger.error('Test entry: %j', { id: 'Foo', data: 7 }), true, 'will log error messages when enabled');
-  t.is(logger.debug('Test entry'), false, 'will not log debug messages when disabled');
+  assert.is(logger.name, 'Foo', 'can create a logger');
+  assert.is(logger.error('Test entry: %j', { id: 'Foo', data: 7 }), true, 'will log error messages when enabled');
+  assert.is(logger.debug('Test entry'), false, 'will not log debug messages when disabled');
 
   LoggerFactory.setLevel(logger, undefined);
   LoggerFactory.GLOBAL_LEVEL = Level.ALL;
 
-  t.equals(logger.level, LoggerFactory.GLOBAL_LEVEL, 'logger will return the GLOBAL_LEVEL when not defined');
-  t.is(logger.debug('Debug test entry'), true, 'logger will respect the GLOBAL_LEVEL when not defined specifically');
-  t.is(logger.trace('Trace test entry'), true, 'logger will print trace level when enabled');
+  assert.equals(logger.level, LoggerFactory.GLOBAL_LEVEL, 'logger will return the GLOBAL_LEVEL when not defined');
+  assert.is(logger.debug('Debug test entry'), true, 'logger will respect the GLOBAL_LEVEL when not defined specifically');
+  assert.is(logger.trace('Trace test entry'), true, 'logger will print trace level when enabled');
 
-  t.end();
+  assert.end();
 });
